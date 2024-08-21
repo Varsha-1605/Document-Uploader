97e64800065ca44606d1dedbea482828a7810fed
Varsha Dewangan <140236223+Varsha-1605@users.noreply.github.com>
2024-08-21 11:41:25 +0530

    Update final_document_uploader.py

--- final_document_uploader.py
+++ final_document_uploader.py
@@ -1291,9 +1291,9 @@
 
 
 
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import sys
 import tempfile
